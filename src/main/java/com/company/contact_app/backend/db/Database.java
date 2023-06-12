bce806f9875b547d47167dee4ac5040d65f417a3
AbdullohAbduganiyev <aliganiyev888@mail.ru>
2023-06-12 17:14:58 +0500

    service begin

--- src/main/java/com/company/contact_app/backend/db/Database.java
+++ src/main/java/com/company/contact_app/backend/db/Database.java
@@ -8,4 +8,11 @@ import java.util.List;
 public interface Database {
     List<User> USERS = new ArrayList<>();
 
+    static void loadData(){
+        User user1 = new User("Davlataliyev Donyor","Donyor","+998931419445");
+        User user2 = new User("Alisher","javabackenddeveloper","+9989900787802");
+        User user3 = new User("Ibrohim Hamidov","IbrohimB29","+998977776399");
+        User user4 = new User("Bakhtiyorov_Jamshid","JamshidB29","+998933138827");
+    }
+
 }
--- src/main/java/com/company/contact_app/backend/service/UserServiceImpl.java
+++ src/main/java/com/company/contact_app/backend/service/UserServiceImpl.java
@@ -1,13 +1,32 @@
 package com.company.contact_app.backend.service;
 
+import com.company.contact_app.backend.db.Database;
+import com.company.contact_app.backend.dto.Repository;
+import lombok.NonNull;
+
+import java.util.Objects;
+
 public class UserServiceImpl implements UserService {
-    @Override
-    public void saveContact(String name, String phoneNumber) {
 
+
+    @Override
+    public Repository saveContact(@NonNull String name, @NonNull String phoneNumber) {
+        try{
+            if (name.isBlank()){
+                return new Repository("User name is required!",false);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
-    public void findNumber(String name) {
-
+    public void findNumber(@NonNull String name) {
+        Database.USERS.stream()
+                .filter(user -> Objects.equals(user.getUsername(),name))
+                .findFirst()
+                .orElse(null);
     }
+
 }
