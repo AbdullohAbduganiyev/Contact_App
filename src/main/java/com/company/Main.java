6a598dc7d1934049662d90e67d9c75accc49e757
AbdullohAbduganiyev <aliganiyev888@mail.ru>
2023-06-12 16:12:05 +0500

    frontend begin

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import com.company.contact_app.frontend.UserUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        UserUI.window();
     }
 }
